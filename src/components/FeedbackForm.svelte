8a17fab75eb1c3f25c6f42e49554e93f61c0cbf4
MrBurp <andreass.ziegler@gmail.com>
2022-11-08 20:11:12 +0100

    btn form

--- src/components/FeedbackForm.svelte
+++ src/components/FeedbackForm.svelte
@@ -1,7 +1,12 @@
 <script>
-  
+  import Card from "./Card.svelte";
+  import Button from "./Button.svelte";
+
+  let text = ''
+  let btnDesabled = true
 </script>
 
+<Card>
 <header>
   <h2>How would you rate our service with us?</h2>
 </header>
@@ -9,10 +14,11 @@
 <form>
   <!-- Rating Select -->
   <div class="input-group">
-    <input type="text" placeholder="Tell us something that keeps you coming back">
-    <button>Send</button>
+    <input type="text" bind:value={text} placeholder="Tell us something that keeps you coming back">
+    <Button disabled={btnDesabled} type="submit">Send</Button>
   </div>
 </form>
+</Card>
 
 <style>
   .input-group{
