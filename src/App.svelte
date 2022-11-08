713c4cd8f1b2d382c2a78c6f1f6c9922a6168c29
MrBurp <andreass.ziegler@gmail.com>
2022-11-08 18:52:30 +0100

    count and average add

--- src/App.svelte
+++ src/App.svelte
@@ -18,13 +18,18 @@ import FeedbackList from './components/FeedbackList.svelte'
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
 	]
+
+	$: count = feedback.length
+	$: average = feedback.reduce((a, {rating})=>a + rating ,0)/ feedback.length
+
 	const deleteFeedback = e => {
-		const itemId =e.detail
+		const itemId = e.detail
 		feedback = feedback.filter((item)=>item.id != itemId)
 		}
 </script>
 
 <main class="container">
+
 	<FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
 	
 </main>
