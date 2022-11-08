eeffb5aefed9e62fe0ea3d705ce8931d46865bee
MrBurp <andreass.ziegler@gmail.com>
2022-11-08 19:11:33 +0100

    add Form component

---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<form>
+  <!-- Rating Select -->
+  <div class="input-group">
+    <input type="text" placeholder="Tell us something that keeps you coming back">
+    <button>Send</button>
+  </div>
+</form>
+
+<style>
+  .input-group{
+
+  }
+
+</style>
